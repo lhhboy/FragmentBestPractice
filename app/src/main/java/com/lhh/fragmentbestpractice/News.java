8e12c71e08cef0ca7aa62d47b4219bf88c37d91a
lhhboy <1603614324@qq.com>
2019-10-05 17:42:31 +0800

    第一行代码fragment

---
+++ app/src/main/java/com/lhh/fragmentbestpractice/News.java
@@ -0,0 +1,21 @@
+package com.lhh.fragmentbestpractice;
+
+public class News {
+    private String tltle;
+    private String content;
+
+    public void setTltle(String tltle) {
+        this.tltle = tltle;
+    }
+
+    public String getTltle() {
+        return tltle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+}
